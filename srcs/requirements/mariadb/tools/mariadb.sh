0002b31ac85c562f0b7e0468a8380f1b9b4dfb77
Agustina Heredia Marengo <agusheredia@Air-de-Agustina.home>
2025-02-08 09:48:27 +0100

    configuracion conexion bd wp

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -8,7 +8,7 @@ chown mysql:mysql /run/mysqld
 
 # Verifica si el directorio de la base de datos no existe
 if [ ! -d /var/lib/mysql/${DB_NAME} ]; then
-    echo "Initializing the database.."
+    echo "Inicializando la base de datos..."
     # Inicializa la base de datos con mysql_install_db
     mysql_install_db --user=mysql --ldata=/var/lib/mysql
 fi
@@ -18,17 +18,15 @@ mysqld_safe --datadir='/var/lib/mysql' &
 # Espera 5 segundos para asegurarse de que el servicio esté listo
 sleep 5
 
-# Verifica si el directorio de la base de datos no existe
-if [ ! -d /var/lib/mysql/${DB_NAME} ]; then
-    # Crea la base de datos
-    mysql -u ${DB_ROOT} -p${DB_ROOT_PASS} -e "CREATE DATABASE ${DB_NAME};"
-    # Crea un nuevo usuario con las credenciales proporcionadas
-    mysql -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}'"
-    # Otorga todos los privilegios al nuevo usuario en la base de datos creada
-    mysql -e "GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}' WITH GRANT OPTION;"
-    # Aplica los cambios de privilegios
-    mysql -e "FLUSH PRIVILEGES;"
-fi
+# Conexión como root para configurar la base de datos y usuarios
+echo "Configurando la base de datos y permisos..."
+mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' WITH GRANT OPTION;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
+FLUSH PRIVILEGES;
+EOF
 
 # Apaga el servicio de MariaDB
 mysqladmin -u ${DB_ROOT} --password=${DB_ROOT_PASS} shutdown
